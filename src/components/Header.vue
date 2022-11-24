95ef9cd1c2dba9e8e78484fe1204437158bd6a13
jabed <jabedzaman2004@gmail.com>
2022-11-24 22:33:52 +0530

    💄 add: Header

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="p-10">
+          <h1 class="flex justify-center font-bold lg:text-6xl text-5xl lg:mt-20 mt-16 text-white">
+            Weather Here 🌡
+          </h1>
+          <p class="flex justify-center font-bold lg:text-2xl text-xl lg:mt-2 mt-1 text-white">
+            Check the weather in your city!! 
+          </p>
+        </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld',
+  }
+  </script>  
