7f62020572bb9c2dc4e95c9849f2b813411e376f
jabed <jabedzaman2004@gmail.com>
2022-11-24 23:07:31 +0530

    :children_crossing: add built by

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,22 @@
 <template>
-    <div class="p-10">
-          <h1 class="flex justify-center font-bold lg:text-6xl text-5xl lg:mt-20 mt-16 text-white">
-            Weather Here 🌡
-          </h1>
-          <p class="flex justify-center font-bold lg:text-2xl text-xl lg:mt-2 mt-1 text-white">
-            Check the weather in your city!! 
-          </p>
-        </div>
-  </template>
+  <div class="p-10">
+    <h1 class="flex justify-center font-bold lg:text-6xl text-5xl lg:mt-20 mt-16 text-white">
+      Weather Here 🌡
+    </h1>
+    <p class="flex justify-end mr-10 pr-10 p-2 mb-10 lg:justify-center lg:ml-28 text-white">
+      - built by
+      <a href="https://jabed.me" class="hover:text-blue-500 mx-1 duration-150 ease-in-out">
+        Jabed
+      </a>
+    </p>
+    <p class="flex justify-center font-bold lg:text-2xl text-xl lg:mt-2 mt-1 text-white">
+      Check the weather in your city!!
+    </p>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'HeaderPage',
-  }
-  </script>  
+<script>
+export default {
+  name: 'HeaderPage',
+}
+</script>  
